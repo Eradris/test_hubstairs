31c04ee955b4756d51d3a31b72c901eda7929d8d
Sebastien <sebastien.bigorne@gmail.com>
2018-07-15 17:36:14 +0200

    last commit

--- src/components/Prime.js
+++ src/components/Prime.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
-import { Grid, Header, Icon } from 'semantic-ui-react'
+import { Grid, Header, Icon, Input, Button } from 'semantic-ui-react'
 
 class Prime extends Component {
+
+	valider(){
+		let num = document.getElementById('num').value
+		console.log(num)
+	}
+
   render() {
     return (
       <Grid centered>
-        <Header as='h2'>
-		  <Icon name='calculator' />
-		  <Header.Content>Prime</Header.Content>
-		</Header>
+        <Grid.Row centered>
+          <Header as='h2'>
+            <Icon name='calculator' />
+            <Header.Content>Prime</Header.Content>
+          </Header>
+        </Grid.Row>
+        <Grid.Row columns={3}>
+          <Grid.Column>
+          	<Input type='number' id='num'/>
+          </Grid.Column>
+          <Grid.Column>
+          	<Button onClick={() => this.valider()}>Valider</Button>
+          </Grid.Column>
+          <Grid.Column>
+          	<h3>Résultat</h3>
+          </Grid.Column>
+        </Grid.Row>
       </Grid>
     );
   }
